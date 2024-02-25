f417445758f51c3839a9cf35944a5692aa4c0a3c
Emil Zi <emil.ziyannurov@mail.ru>
2024-02-25 17:22:30 +0300

    Добавлен код для вычисления площади фигур

---
+++ GeometryLibrary.cs
@@ -0,0 +1,49 @@
+using System;
+
+namespace GeometryLibrary
+{
+    public interface IShape
+    {
+        double CalculateArea();
+    }
+
+    public class Circle : IShape
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public double CalculateArea()
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+
+    public class Triangle : IShape
+    {
+        private double side1, side2, side3;
+
+        public Triangle(double side1, double side2, double side3)
+        {
+            this.side1 = side1;
+            this.side2 = side2;
+            this.side3 = side3;
+        }
+
+        public double CalculateArea()
+        {
+            double s = (side1 + side2 + side3) / 2;
+            return Math.Sqrt(s * (s - side1) * (s - side2) * (s - side3));
+        }
+
+        public bool IsRightAngled()
+        {
+            double[] sides = { side1, side2, side3 };
+            Array.Sort(sides);
+            return sides[2] * sides[2] == sides[0] * sides[0] + sides[1] * sides[1];
+        }
+    }
+}
